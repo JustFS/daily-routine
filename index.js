91609ecfc2decc44e69c57e8cac998926c3724ef
Julien Az <julien.azbrg@gmail.com>
2021-10-29 18:57:54 +0200

    paj 2.0

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const main = document.querySelector("main");
 const basicArray = [
   { pic: 0, min: 1 },
+  { pic: 2, min: 1 },  
   { pic: 1, min: 1 },
+  { pic: 3, min: 2 },
   { pic: 2, min: 1 },
-  { pic: 3, min: 1 },
-  { pic: 4, min: 1 },
   { pic: 5, min: 1 },
-  { pic: 6, min: 1 },
-  { pic: 7, min: 1 },
+  { pic: 7, min: 2 },
   { pic: 8, min: 1 },
-  { pic: 9, min: 1 },
+  { pic: 2, min: 1 },
+  { pic: 9, min: 2 },
 ];
 let exerciceArray = [];
 
